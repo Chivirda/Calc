5099ae4906292da8df77df5ad06f5748d9e6b8fe
Serg Ch <chivirdos@gmail.com>
2019-10-09 22:30:12 +0300

    Add checkString method

---
+++ src/ArabCalc.java
@@ -0,0 +1,5 @@
+public class ArabCalc {
+    public void arabCalc(String inputString) {
+
+    }
+}
--- src/Calc.java
+++ src/Calc.java
@@ -1,5 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calc {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String inputString = reader.readLine();
+
+            checkString(inputString);
+
+        }
+    }
+
+    private static void checkString(String inputString) {
+        Pattern arabPattern = Pattern.compile("\\d");
+        Matcher arabMatcher = arabPattern.matcher(inputString);
+        Pattern romePattern = Pattern.compile("[IVX]");
+        Matcher romeMatcher = romePattern.matcher(inputString);
+
+        if (arabMatcher.find() && romeMatcher.find()) {
+            throw new ArithmeticException();
+        } else if (romeMatcher.find()) {
+            System.out.println("Rome Calc");
+        } else if (arabMatcher.find()) {
+            System.out.println("Arab Calc");
+        }
 
     }
 }
