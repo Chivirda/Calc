46e52f6eb899f06e4ec27b6f11b26e1f7245df70
Sergey Chivirda <chivirdos@gmail.com>
2019-10-11 20:42:01 +0300

    Move enum Roman into separate file

--- src/RomeCalc.java
+++ src/RomeCalc.java
@@ -1,22 +1,6 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-enum Roman {
-    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10), XI(11), XII(12), XIII(13),
-    XIV(14), XV(15), XVI(16), XVII(17), XVIII(18), XIX(19), XX(20), XXI(21), XXIV(24), XXV(25), XXVII(27), XXX(30),
-    XXXII(32), XXXV(35), XXXVI(36), XL(40), XLII(42), XLV(45), XLVIII(48), XLIX(49), L(50), LIV(54), LVI(56), LX(60),
-    LXIII(63), LXIV(64), LXX(70), LXXII(72), LXXX(80), LXXXI(81), XC(90), C(100);
-    private final int value;
-
-    Roman(int value) {
-        this.value = value;
-    }
-
-    int toInt() {
-        return value;
-    }
-}
-
 class RomeCalc {
     static void romeCalc(String inputString) throws IOException {
         ArrayList<Integer> numbers = new ArrayList<>();
@@ -29,8 +13,10 @@ class RomeCalc {
 
         int result = Helper.checkMath(inputString, numbers);
 
-
-
-        System.out.println(result);
+        for (Roman num : Roman.values()) {
+            if (num.toInt() == result) {
+                System.out.println(num.name());
+            }
+        }
     }
 }
